26c62c7a3eec87837acaf2fa31f79ce733ac54e1
aerolicavoli <aerolicavoli@gmail.com>
2025-05-25 19:13:38 -0600

    Update buildspec for motor control

--- Pancake_esp/main/pancake_esp_main.cpp
+++ Pancake_esp/main/pancake_esp_main.cpp
@@ -1,4 +1,5 @@
-#include "MotorControl.h"
+// #include "MotorControl.h"
+
 #include "StepperMotor.h"
 #include "defines.h"
 
@@ -9,22 +10,24 @@ extern "C"
 #include "Safety.h"
 #include "TlmPublisher.h"
 #include "UI.h"
+#include "MotorControl.h"
+
 
     void app_main(void)
     {
+        esp_log_level_set("wifi", ESP_LOG_WARN);
         // Initialize the tasks
         PiUIInit();
         SafetyInit();
-        // MotorControlInit();
-        // UIInit();
+        MotorControlInit();
+        //  UIInit();
 
         // Start the tasks
         SafetyStart();
-        //    MotorControlStart();
+        MotorControlStart();
         PiUIStart();
         // UIStart();
-        // TlmPublisherStart();
 
-        TlmPublisherInit();
+        TlmPublisherInitAndStart();
     }
 }
